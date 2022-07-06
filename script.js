78686dc80cac0005b5d16e9b19a89d9488f4677c
Adi <skb.sharafat@gmail.com>
2022-07-06 23:39:03 +0600

    second commit

--- script.js
+++ script.js
@@ -2,10 +2,40 @@ let ulEl = document.querySelector("#unorderedList");
 let inp = document.getElementById("inp");
 let err = document.getElementById("error");
 let but = document.getElementById("in");
+let dlt = document.getElementById("dlt");
 
 let arr = [];
 let count = 0;
 
+// localStorage.setItem("mytag", "hello");
+// localStorage.clear();
+
+
+
+
+function gotcha(){
+    let myLeadsToLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+    if(myLeadsToLocalStorage){
+        arr = myLeadsToLocalStorage;
+        rendr(arr);
+    }
+}
+
+gotcha();
+
+function rendr(arr){
+    for(let j = 0; j<(arr.length); j++){
+        var listin = `<li><a href = '${arr[j]}'> ${arr[j]}</a></li>`
+        ulEl.innerHTML += listin;
+    }
+};
+
+function rendre(arr){
+    for(let j = 0; j<(arr.length); j++){
+        var listin = `<li><a target='_blank' href = '${arr[j]}'> ${arr[j]}</a></li>`
+    }
+    ulEl.innerHTML += listin;
+};
 
 
 but.addEventListener("click",()=>{
@@ -24,19 +54,27 @@ but.addEventListener("click",()=>{
                 console.log("no matches found with previous bookmarks!!")
             }
         }
+
         if(count == 0){
             arr.push(inpt);
-            for(let j = 0; j<(arr.length); j++){
-                var listin = `<li><a href = '${arr[j]}'> ${arr[j]}</a></li>`
-            }
-            ulEl.innerHTML += listin;
+            localStorage.setItem("myLeads",JSON.stringify(arr));
+            rendre(arr)
+            
         }
     }
     inp.value = '';
     count = 0;
     
 })
+
 inp.addEventListener("change", ()=>{
     error.textContent = ''
+})
+
+dlt.addEventListener("dblclick", ()=>{
+    localStorage.clear();
+    arr = [];
+    rendr(arr);
+    window.location.reload();
 })
 
